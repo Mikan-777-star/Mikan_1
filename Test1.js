28097736a0054e94756124b418c9371a74ff9ffa
Mikan-777-star <79889425+Mikan-777-star@users.noreply.github.com>
2021-03-03 08:47:18 +0900

    数字で打ち込めないかね？

--- Test1.js
+++ Test1.js
@@ -5,15 +5,7 @@
 const startbutton = document.getElementById("start");
 startbutton.onclick = () =>{
     function test(s){
-        let test = 0;
-        switch (s) {
-            case 'S':test = 50;
-                break;
-            case 'M':test = 100;
-                break;
-            default:test = 200;
-                break;
-        }
+        let test = s;
         let str = "<h3>ま゛";
         for(let i = 2; i <= test - 3; i++){
             str += "ぅ゛";
@@ -24,7 +16,7 @@ startbutton.onclick = () =>{
         }
         return str + "</h3>";
     }
-    document.getElementById('ans').innerHTML = test(window.prompt('ボタンを押したな！さあ長さを選べ！[S/M/L]'));
+    document.getElementById('ans').innerHTML = test(window.prompt('ボタンを押したな！さあ長さを数字で書け！'));
     var s = '<h6>おまけのきららシャミ桃</h6><img src="monmo26980.jpg"><img src="Syamiko.jpg">';
     s += '<br>made by <a href = "https://twitter.com/mikan26980">大彗星みかん</a>';
     document.getElementById('26980').innerHTML = s;
