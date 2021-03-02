bba7739ed0d870aef29f2f485a512355391566d0
Mikan-777-star <79889425+Mikan-777-star@users.noreply.github.com>
2021-03-02 21:54:27 +0900

    改行を消そう

--- Test1.js
+++ Test1.js
@@ -14,12 +14,10 @@ startbutton.onclick = () =>{
         let str = "<h3>ま゛";
         for(let i = 2; i <= test + 1; i++){
             str += "ぅ゛";
-            if(i % 25 == 0)str += '<br>';
         }
         str += '<br>';
         for(let i = 1; i <= (test + 1) / 2; i++){
             str += "♡゛";
-            if(i % 25 == 0)str += '<br>';
         }
         return str + "</h3>";
     }
