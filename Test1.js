669f71a051aeb734feb332e7ebc29c9d29f5b62b
Mikan-777-star <79889425+Mikan-777-star@users.noreply.github.com>
2021-03-03 15:39:02 +0900

    隠し要素の追加

--- Test1.js
+++ Test1.js
@@ -5,6 +5,9 @@
 const startbutton = document.getElementById("start");
 startbutton.onclick = () =>{
     function test(s){
+        if(s === 56562||s === 26980 ){
+            return '<a href ="https://twitter.com/hekisaki_umau/status/1366977824075042826">隠しコマンドを入力したね！</a>';
+        }
         let test = s;
         let str = "<h3>ま゛";
         for(let i = 2; i <= test - 3; i++){
