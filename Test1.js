4e2de397cc7e3670f1cc6c3ad6a4afc1f6e59848
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-05 13:40:50 +0900

    CSS作ったり画像載せたり
    バエル

--- Test1.js
+++ Test1.js
@@ -13,7 +13,6 @@ startbutton.onclick = () =>{
         for(let i = 2; i <= test - 3; i++){
             str += "ぅ゛";
         }
-        str += '<br>';
         for(let i = 1; i <= (test - 5 )/ 2; i++){
             str += "♡゛";
         }
@@ -21,6 +20,6 @@ startbutton.onclick = () =>{
     }
     document.getElementById('ans').innerHTML = test(window.prompt('ボタンを押したな！さあ長さを数字で書け！'));
     var s = '<h6>おまけのきららシャミ桃</h6><img src="monmo26980.jpg"><img src="Syamiko.jpg">';
-    s += '<br>made by <a href = "https://twitter.com/mikan26980">大彗星みかん</a>';
+    s += '<br>made by <a href = "https://twitter.com/mikan26980" id = "umau">大彗星みかん</a>';
     document.getElementById('26980').innerHTML = s;
 }
